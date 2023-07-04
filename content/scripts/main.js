2043077e064818b97821e0edf28fabe5bb002585
Cássio dos Santos <cassiovsd@hotmail.com>
2023-07-04 19:55:39 -0300

    inicio do merge

--- content/scripts/main.js
+++ content/scripts/main.js
@@ -1,38 +1,60 @@
 let navItensContent;
-let showAllElement
+let showAllElement;
+let categoriaSelecionada;
+let baseURL = "file:///C:/Users/Adm/Desktop/West-Devs-entrega-final-Culturalize";
+let path = "/content/views/catalog/";
 
 function init() {
-    navItensContent[0].classList.add("selected");
+    selectCategoria(navItensContent[0]);
 
-    if(navItensContent[0].id !== "obras") {
+    if (navItensContent[0].id !== "obras") {
         showAllElement.innerText = `Confira todos os ${navItensContent[0].id}`
     } else {
         showAllElement.innerText = `Confira todas as ${navItensContent[0].id}`
     }
-    
 
     navItensContent.forEach(e => {
         e.addEventListener("click", function () {
             selectCategoria(e);
         });
     });
+
+    showAllElement.addEventListener("click", function () {
+        goToCatalog(categoriaSelecionada);
+    });
 }
 
 function selectCategoria(item) {
     navItensContent.forEach(e => {
-        if(e.classList.contains("selected")) {
+        if (e.classList.contains("selected")) {
             e.classList.remove("selected")
         }
     });
 
     item.classList.add("selected");
 
-    if(item.id !== "obras") {
+    if (item.id !== "obras") {
         showAllElement.innerText = `Confira todos os ${item.id}`
     } else {
         showAllElement.innerText = `Confira todas as ${item.id}`
     }
+
+    categoriaSelecionada = item.id;
 }
+
+function goToCatalog(tpCategoria) {
+    switch (tpCategoria) {
+        case "obras":
+            window.location.href = baseURL + path + "obras.html";
+            break;
+        case "eventos":
+            window.location.href = baseURL + path + "events.html";
+            break;
+        case "cursos":
+            window.location.href = baseURL + path + "courses.html";
+            break;
+    }
+};
 
 document.addEventListener("DOMContentLoaded", function () {
     navItensContent = document.querySelectorAll(".nav-item-content");
