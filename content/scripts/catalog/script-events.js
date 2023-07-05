aa2bf048a7aa9cbfd0146e9ab17418ebf980a0d1
Cássio dos Santos <cassiovsd@hotmail.com>
2023-07-04 21:47:41 -0300

    pequenas correções

--- content/scripts/catalog/script-events.js
+++ content/scripts/catalog/script-events.js
@@ -75,6 +75,109 @@ let EVENTOS = [
     }
 ]
 
+
+let DETALHES_EVENTOS = [
+    {
+        id: 1,
+        url_img: baseURL + "/content/imgs/02-Eventos/01-Eventos-Exposicoes/ExposicaoFotografia.jpg",
+        nome: "Exposição de fotografia",
+        descricao: "A exposição é uma jornada imersiva que convida os visitantes a explorarem diferentes formas de expressão artística. A exposição apresenta uma variedade de obras de arte, incluindo pinturas, esculturas, fotografias e instalações, criadas por artistas renomados e emergentes. Cada sala da exposição é cuidadosamente projetada para proporcionar uma experiência única, com iluminação e disposição das obras que realçam sua beleza e significado.",
+        artista: "Clara",
+        data: "22-04-2023 14:30",
+        local: "Avenida 1,  7, Foz do Iguaçu-PR",
+        valor: "R$ 100,00",
+        event_id: 1
+    },
+    {
+        id: 2,
+        url_img: baseURL + "/content/imgs/02-Eventos/01-Eventos-Exposicoes/ExposicaoPinturas.jpg",
+        nome: "Exposição de pinturas",
+        descricao: "A exposição é uma jornada imersiva que convida os visitantes a explorarem diferentes formas de expressão artística. A exposição apresenta uma variedade de obras de arte, incluindo pinturas, esculturas, fotografias e instalações, criadas por artistas renomados e emergentes. Cada sala da exposição é cuidadosamente projetada para proporcionar uma experiência única, com iluminação e disposição das obras que realçam sua beleza e significado.",
+        artista: "Gustavo",
+        data: "22-04-2023 14:30",
+        local: "Avenida 1,  7, Foz do Iguaçu-PR",
+        valor: "R$ 100,00",
+        event_id: 2
+    },
+    {
+        id: 3,
+        url_img: baseURL + "/content/imgs/02-Eventos/01-Eventos-Exposicoes/ExposicaoQuadros.jpg",
+        nome: "Exposição de arte",
+        descricao: "A exposição é uma jornada imersiva que convida os visitantes a explorarem diferentes formas de expressão artística. A exposição apresenta uma variedade de obras de arte, incluindo pinturas, esculturas, fotografias e instalações, criadas por artistas renomados e emergentes. Cada sala da exposição é cuidadosamente projetada para proporcionar uma experiência única, com iluminação e disposição das obras que realçam sua beleza e significado.",
+        artista: "Andréa",
+        data: "22-04-2023 14:30",
+        local: "Avenida 1,  7, Foz do Iguaçu-PR",
+        valor: "R$ 100,00",
+        event_id: 3
+    },
+    {
+        id: 4,
+        url_img: baseURL + "/content/imgs/02-Eventos/02-Eventos-Musicas/musica.jpg",
+        nome: "Apresentação Orquestra Sinfônica ",
+        descricao: "Com uma atmosfera envolvente e energética, o evento proporciona uma variedade de estilos musicais, incluindo rock, pop, jazz, música clássica e world music. Além das apresentações, o evento também oferece espaços interativos, como estandes de venda de discos, alimentos e bebidas, proporcionando uma experiência completa para os amantes da música.",
+        artista: "Orquestra Sinfônica",
+        data: "22-04-2023 14:30",
+        local: "Avenida 1,  7, Foz do Iguaçu-PR",
+        valor: "R$ 100,00",
+        event_id: 4
+    },
+    {
+        id: 5,
+        url_img: baseURL + "/content/imgs/02-Eventos/03-Eventos-Apresentacoes/cinema.jpg",
+        nome: "Mostra de Cinema Iguaçu",
+        descricao: "O evento de cinema é uma experiência cinematográfica envolvente que reúne amantes da sétima arte em um ambiente inspirador. Com uma seleção cuidadosa de filmes de diferentes gêneros, o evento oferece uma programação diversificada que abrange desde clássicos até produções contemporâneas e independentes.",
+        artista: "Luana",
+        data: "22-04-2023 14:30",
+        local: "Avenida 1,  7, Foz do Iguaçu-PR",
+        valor: "R$ 100,00",
+        event_id: 5
+    },
+    {
+        id: 6,
+        url_img: baseURL + "/content/imgs/02-Eventos/03-Eventos-Apresentacoes/concert.jpg",
+        nome: "Festival de Música Guarani",
+        descricao: "Com uma atmosfera envolvente e energética, o evento proporciona uma variedade de estilos musicais, incluindo rock, pop, jazz, música clássica e world music. Além das apresentações, o evento também oferece espaços interativos, como estandes de venda de discos, alimentos e bebidas, proporcionando uma experiência completa para os amantes da música.",
+        artista: "Rogério",
+        data: "22-04-2023 14:30",
+        local: "Avenida 1,  7, Foz do Iguaçu-PR",
+        valor: "R$ 100,00",
+        event_id: 6,
+    },
+    {
+        id: 7,
+        url_img: baseURL + "/content/imgs/02-Eventos/03-Eventos-Apresentacoes/danca.jpg",
+        nome: "Apresentação de Dança",
+        descricao: "O evento de dança é uma celebração da expressão corporal e da arte do movimento. Realizado em um ambiente acolhedor e inspirador, o evento reúne bailarinos profissionais, grupos de dança e artistas talentosos de diferentes estilos, como ballet, dança contemporânea, dança de salão e danças folclóricas.",
+        artista: "Balé de Foz do Iguaçu",
+        data: "22-04-2023 14:30",
+        local: "Avenida 1,  7, Foz do Iguaçu-PR",
+        valor: "R$ 100,00",
+        event_id: 7
+    },
+    {
+        id: 8,
+        url_img: baseURL + "/content/imgs/02-Eventos/04-Eventos-Literatura/literatura1.jpg",
+        nome: "Lançamento do livro Naipi",
+        descricao: "O evento de lançamento do livro Naipi é uma celebração literária que reúne autores, leitores e entusiastas da literatura em um ambiente acolhedor. Com um clima de expectativa e empolgação, o evento marca o momento em que a obra ganha vida e é apresentada ao público pela primeira vez.No evento, o autor tem a oportunidade de compartilhar sua jornada criativa e os bastidores da escrita, proporcionando insights sobre o processo de criação da obra. Os leitores têm a chance de conhecer de perto o autor, obter uma cópia autografada do livro e interagir com outros fãs da obra.",
+        artista: "Juliana",
+        data: "22-04-2023 14:30",
+        local: "Avenida 1,  7, Foz do Iguaçu-PR",
+        valor: "R$ 100,00",
+        event_id: 8
+    },
+    {
+        id: 9,
+        url_img: baseURL + "/content/imgs/02-Eventos/04-Eventos-Literatura/literatura2.jpg",
+        nome: "Encontro Internacional do Clube do Livro",
+        descricao: "O Encontro Internacional do Clube do Livro é um evento emocionante que reúne entusiastas da leitura de todo o mundo para celebrar a paixão pelos livros e promover o intercâmbio literário. Realizado em um ambiente acolhedor e culturalmente diversificado, o evento oferece uma oportunidade única de conectar pessoas que compartilham o amor pela literatura.",
+        artista: "Vários",
+        data: "22-04-2023 14:30",
+        local: "Avenida 1,  7, Foz do Iguaçu-PR",
+        valor: "R$ 100,00",
+        event_id: 9
+    }
+]
+
 let allEventsButtons = document.getElementById("events-article-button");
 
 let exposicoesButton = document.createElement("button");
